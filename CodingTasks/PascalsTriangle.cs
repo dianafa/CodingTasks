a306c1143460b6cebb2715340b018a0499df9855
dianafa <diana.falkowska@gmail.com>
2015-03-12 23:18:32 +0100

    init

---
+++ CodingTasks/PascalsTriangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks
+{
+    public class PascalsTriangle
+    {
+        /*public IList<IList<int>> Generate(int numRows)
+        {
+         
+        }*/
+
+        public List<List<int>> Generate(int numRows)
+        {
+            List<int> row = new List<int>();
+            List<List<int>> setOfRows = new List<List<int>>();
+            row.Add(1);
+            row.Add(5);
+            setOfRows.Add(row);
+            return setOfRows;
+        }
+
+        void PrintResult(List<List<int>> listlist)
+        {
+            ;
+        }
+    }
+}
---
+++ CodingTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PascalsTriangle PT = new PascalsTriangle();
+            var result = PT.Generate(5);
+            Console.WriteLine("wynik:" + result);
+            Console.ReadLine();
+        }
+    }
+}
