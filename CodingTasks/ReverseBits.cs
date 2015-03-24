e770cbcd0269905a63f4507b789519a5d8fdc81b
dianafa <diana.falkowska@gmail.com>
2015-03-24 19:44:52 +0100

    ReverseBits class

---
+++ CodingTasks/ReverseBits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks
+{
+    /*
+     * Reverse bits of a given 32 bits unsigned integer.
+     */
+    class ReverseBits
+    {
+        public uint reverseBits(uint n) {
+            uint r = 0;
+            uint m = (uint)Math.Pow(2, 31);
+            
+            while (n > 0)
+            {
+                if (n % 2 == 1) {
+                    r += m;
+                }
+                n >>= 1;
+                m >>= 1;
+            }
+            Console.WriteLine("Reversed number: " + r);
+            return r;
+        }
+    }
+}
