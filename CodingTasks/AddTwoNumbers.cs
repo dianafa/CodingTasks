4eaf9f60096b681a273775cf5c316f4ff8654e08
dianafa <diana.falkowska@gmail.com>
2015-03-26 08:55:45 +0100

    AddTwoNumbers

---
+++ CodingTasks/AddTwoNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks
+{
+    /*
+     * You are given two linked lists representing two non-negative numbers.
+     * The digits are stored in reverse order and each of their nodes contain a single digit.
+     * Add the two numbers and return it as a linked list.
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4 -> 9)
+     * Output: 7 -> 0 -> 8 -> 9
+     */
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    class AddTwoNumbers
+    {
+        public ListNode AddsTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode list = new ListNode(-1);
+            ListNode result = list;
+            int tmp = 0;
+
+            while (l1 != null || l2 != null || tmp > 0)
+            {
+                if (l1 == null) l1 = new ListNode(0);
+                if (l2 == null) l2 = new ListNode(0);
+
+                int sum = l1.val + l2.val + tmp;
+                tmp = 0;
+                ListNode node3 = new ListNode(sum % 10);
+                tmp = sum / 10;
+                l1 = l1.next;
+                l2 = l2.next;
+
+                if (list.val == -1)
+                {
+                    list = node3;
+                    result = list;
+                }
+                else
+                {
+                    list.next = node3;
+                    list = list.next;
+                }
+            }
+            printResult(result);
+            return result;
+        }
+
+        public void printResult(ListNode list)
+        {
+            while (list.next != null)
+            {
+                Console.Write(list.val + " -> ");
+                list = list.next;
+            }
+            Console.WriteLine(list.val);
+        }
+    }
+}
--- CodingTasks/Program.cs
+++ CodingTasks/Program.cs
@@ -23,6 +23,22 @@ namespace CodingTasks
             ReverseBits RB = new ReverseBits();
             RB.reverseBits(43261596);
 
+            AddTwoNumbers ATN = new AddTwoNumbers();
+            ListNode la1 = new ListNode(2);
+            ListNode la2 = new ListNode(4);
+            ListNode la3 = new ListNode(3);
+            ListNode lb1 = new ListNode(5);
+            ListNode lb2 = new ListNode(6);
+            ListNode lb3 = new ListNode(4);
+            ListNode lb4 = new ListNode(9);
+            la1.next = la2;
+            la2.next = la3;
+            lb1.next = lb2;
+            lb2.next = lb3;
+            lb3.next = lb4;
+            ATN.AddsTwoNumbers(la1, lb1);
+            ATN.AddsTwoNumbers(new ListNode(1), new ListNode(1));
+
             Console.ReadLine();
         }
     }
